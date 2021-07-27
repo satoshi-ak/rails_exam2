ae312c41f3ba5bd7ee2d2e550cfc3b3e09888026
satoshi aoki <satozi.ak.3250@gmail.com>
2021-07-27 19:53:46 +0900

    css修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,19 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
   skip_before_action :login_required, only: [:new, :create, :show, :edit, :confirm, :destroy]
-  # GET /pictures or /pictures.json
+
   def index
     @pictures = Picture.all
   end
 
-  # GET /pictures/1 or /pictures/1.json
+
   def show
     @picture = Picture.find(params[:id])
     @comment = Comment.new
     @comments = @picture.comments
   end
 
-  # GET /pictures/new
+
   def new
     if params[:back]
     @picture = Picture.new(picture_params)
@@ -26,12 +26,12 @@ def confirm
   @picture = current_user.pictures.build(picture_params)
   @picture.users_id = current_user.id
   render :new if @picture.invalid?
-  # GET /pictures/1/edit
+
 end
   def edit
     @picture = Picture.find(params[:id])
   end
-  # POST /pictures or /pictures.json
+
   def create
     @picture = current_user.pictures.build(picture_params)
     @picture.user_id = current_user.id
@@ -45,7 +45,7 @@ end
       end
     end
   end
-  # PATCH/PUT /pictures/1 or /pictures/1.json
+
   def update
     respond_to do |format|
       if @picture.update(picture_params)
@@ -58,7 +58,7 @@ end
     end
   end
 
-  # DELETE /pictures/1 or /pictures/1.json
+
   def destroy
     @picture.destroy
     respond_to do |format|
@@ -68,12 +68,12 @@ end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_picture
       @picture = Picture.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+  
    def picture_params
       params.require(:picture).permit(:image, :image_cache, :post)
   end
