ca318cc8055139b822d101933d9e04288791d5aa
satoshi aoki <satozi.ak.3250@gmail.com>
2021-07-28 01:13:34 +0900

    エラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,3 +15,5 @@ end
 private
 def comment_params
   params.require(:comment).permit(:content)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :pictures do
-  resources :comments, only: [:create] 
+  resources :comments, only: [:create]
     collection do
       post :confirm
     end
