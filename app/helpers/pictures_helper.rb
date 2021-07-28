551ff049a698e1913c564bb0689c8d5abf14ce6f
satoshi aoki <satozi.ak.3250@gmail.com>
2021-07-29 01:09:49 +0900

    エラー修正

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,5 +1,5 @@
 module PicturesHelper
-  def confirm_pictures_path
+  def  choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
       confirm_pictures_path
     elsif action_name == 'edit'
